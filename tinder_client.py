f291336509c6516fbaf98d0044b66dc4a36b9d38
NakZMichael <nakazato.muka@gmail.com>
2020-09-07 20:57:38 +0900

    セル形式のpythonファイルに変更

--- tinder_client.py
+++ tinder_client.py
@@ -1,58 +1,80 @@
+# %%
 from selenium import webdriver
 import chromedriver_binary
 import requests
 import json
 import time
+# %%
+json_open = open('profile.json', 'r')
+json_load = json.load(json_open)
+userdata = json_load["userdata"]
 
+# %%
 options = webdriver.ChromeOptions()
 
 options.add_argument(
-    '--user-data-dir='
+    '--user-data-dir='+ userdata
 )
-options.add_argument(
-    '--headless'
-)
+##このオプションをつけると画面上でブラウザを立ち上げなくて済む
+# options.add_argument(
+#     '--headless'
+# )
 
 options.add_argument("--remote-debugging-port=9222") 
 options.add_argument('--no-sandbox')
 options.add_argument('--disable-dev-shm-usage')
 
+# %%
 driver = webdriver.Chrome(options=options)
 
+# %%
 driver.get("https://tinder.com/app/recs")
 
+# %%
+BASE_URL = "https://api.gotinder.com/"
+phone_number =  json_load["phonenumber"] #input("Enter your phone number: ")
+session = requests.session()
+# Request OTP verification code
+url = BASE_URL + "v2/auth/sms/send?auth_type=sms"
+body = {"phone_number": phone_number}
+session.post(url, body )
+
+# %%
 api_key = driver.execute_script("return localStorage.getItem('TinderWeb/APIToken')")
-print("api_key:", api_key)
+session = requests.Session()
+headers = {
+    "X-Auth-Token": api_key,
+    "Content-Type": "application/json"
+}
 
-with requests.Session() as session:
-    headers = {
-        "X-Auth-Token": api_key,
-        "Content-Type": "application/json"
-    }
+session.headers.update(headers)
+# %%
+count = 0 
 
-    session.headers.update(headers)
+#一回でいいねする上限
+count_mux = 100
 
+for n in range(6):
     # 女の子リスト取得
     users = session.get("https://api.gotinder.com/v2/recs/core?locale=ja")
 
-    count = 0 
     for user in json.loads(users.text)["data"]["results"]:
         count += 1
 
-        # 1秒待たせて手動っぽくする
-        time.sleep(1)
+        # 0.1秒待たせて手動っぽくする
+        time.sleep(0.1)
+
         print("count:", count)
 
         user_id = user["user"]["_id"]
-        print(user_id)
+        # print(user_id)
 
         # いいね!
         session.get("https://api.gotinder.com/like/{}?locale=ja".format(user_id))
-
         # 一回でいいねする上限
-        if count == 20:
+        if count == count_mux:
             break
-
+    if count == count_mux:
+        break
+# %% 
 driver.quit()
-
-
